a2df400819a4e219476c00cd0f1b69d446b827e0
Ranvir Chhina <ranvir.chhina@lodestonesoftware.com>
2018-09-25 11:28:03 -0700

    Initial Commit

---
+++ codingBatObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  codingBatObjectiveC
+//
+//  Created by Ranvir Chhina on 9/25/18.
+//  Copyright © 2018 Ranvir Chhina. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
